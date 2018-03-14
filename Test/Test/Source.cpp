61630be19e5f5e685aa16f965ccda7050d8c206a
Tommaso Brega <TBrega@uclan.ac.uk>
2018-03-14 15:31:27 +0000

    updated code

---
+++ Test/Test/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int result;
+	a = 4;
+	b = 6;
+	result = a * b;
+	cout << "The result is ";
+	cout << result << endl;
+
+	system("pause");
+}
+
+
